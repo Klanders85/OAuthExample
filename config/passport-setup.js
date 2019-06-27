8946be71ebc9dbb38f3e93dedc5ee5717c3c939c
Kelly D. Landers <kelly.landers@aa.com>
2019-06-27 14:14:45 -0500

    add check for existing user and create new user if they are new

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -12,14 +12,22 @@ passport.use(
             clientSecret: keys.google.clientSecret
         },
         (accessToken, refreshToken, profile, done) => {
-            //passport callback function
-
-            //create a new user to store in DB
-            new User({
-                googleId: profile.id,
-                userName: profile.displayName
-            }).save().then((newUser) => {
-                console.log("New user " + newUser + " created");
+            //check if user already exists in the DB
+            User.findOne({
+                googleId: profile.id
+            }).then((currentUser) => {
+                if (currentUser) {
+                    //already has a profile stored
+                    console.log("Welcome back " + currentUser.userName + "!");
+                } else {
+                    //create a new user and store in DB
+                    new User({
+                        googleId: profile.id,
+                        userName: profile.displayName
+                    }).save().then((newUser) => {
+                        console.log("New user " + newUser + " created");
+                    });
+                }
             });
         }
     )
