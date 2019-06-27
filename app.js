37716c84f5d8b4eb7a7138362f0acb1cefff61ba
Kelly D. Landers <kelly.landers@aa.com>
2019-06-27 14:49:36 -0500

    add cookie and passport set-up

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const keys = require('./config/keys');
 const authRoutes = require('./routes/auth-routes');
@@ -10,6 +12,16 @@ const passportSetup = require('./config/passport-setup');
 
 //set up view engine
 app.set('view engine', 'ejs');
+
+//set up cookies
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+
+//init Passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 //set up routes
 app.use('/auth', authRoutes);
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,18 @@ const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/User');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then((user) => {
+            done(null, user);
+        });
+});
+
+
 passport.use(
     new GoogleStrategy(
         {
@@ -13,22 +25,24 @@ passport.use(
         },
         (accessToken, refreshToken, profile, done) => {
             //check if user already exists in the DB
-            User.findOne({
-                googleId: profile.id
-            }).then((currentUser) => {
-                if (currentUser) {
-                    //already has a profile stored
-                    console.log("Welcome back " + currentUser.userName + "!");
-                } else {
-                    //create a new user and store in DB
-                    new User({
-                        googleId: profile.id,
-                        userName: profile.displayName
-                    }).save().then((newUser) => {
-                        console.log("New user " + newUser + " created");
-                    });
-                }
-            });
+            User.findOne({ googleId: profile.id })
+                .then((currentUser) => {
+                    if (currentUser) {
+                        //already has a profile stored
+                        console.log("Welcome back " + currentUser.userName + "!");
+                        done(null, currentUser);
+                    } else {
+                        //create a new user and store in DB
+                        new User({
+                            googleId: profile.id,
+                            userName: profile.displayName
+                        }).save()
+                            .then((newUser) => {
+                                console.log("New user " + newUser + " created");
+                                done(null, newUser);
+                            });
+                    }
+                });
         }
     )
 );
